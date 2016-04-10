026edde6c69415faddd8d0e34bdf03581cd925f0
robert <robert@monetsu.com>
2016-04-10 14:26:58 +0200

    Basic vote action filters based on status and ownership.

--- modules/voteItems/voteInfo.js
+++ modules/voteItems/voteInfo.js
@@ -15,26 +15,50 @@ if (Meteor.isClient) {
       return votesCollection.findOne({ _id:recordId });
     },
 
-    voteStatus: function () {
-
+    isVoteOwner : function () {
+      // return if the vote is owned by the user
       var recordId = Router.current().params._id;
-      // Add check for ownership of vote in addition to below
-      // also what happens when there is no status set?
-      var profileVoted =  resultsCollection.findOne({voteId : recordId});
 
-      // if (profileVoted.status) {
-      //   var status = {};
-      //   status[profileVoted.status] = true;
-      // } else {
-      //   var status = "new";
-      // };
+      return votesCollection.findOne({ _id:recordId, createdBy: Meteor.userId()})
 
-      var status = "new";
+    },
+
+    ballotStatus: function () {
+
+      // This is so we can set the "next action" UI
+      // component to give appropriate vote options
+      // to the user depending on their account's
+      // relationship to the vote.
+      var recordId = Router.current().params._id;
+      console.log("the _id of the vote: ");
+      console.log(recordId);
+      // Add check for ownership of vote in
+      // addition to below
+      // TODO: what happens when there is no status set?
+      var profileVoted = resultsCollection.findOne({voteId:recordId, createdBy:Meteor.userId()});
+
+      // var ballotStatus = profileVoted.ballotStatus;
+      console.log(profileVoted);
+
+      var theStatus = {};
+      // More conditionals are needed to check
+      // for ownership and published status.
+      if (profileVoted) {
+        console.log("the stored status");
+        console.log(profileVoted.ballotStatus);
+        theStatus[profileVoted.ballotStatus] = true;
+      } else {
+        console.log("not status, new ballot required");
+        theStatus.noBallot = true;
+      };
+
+      // We are overriding this helper to debug.
+      // var status = "new";
 
       console.log("This is the status of this vote");
-      console.log(status);
+      console.log(theStatus);
 
-      return status;
+      return theStatus;
     }
   });
 
@@ -137,16 +161,21 @@ if (Meteor.isClient) {
       //   {'profile.votes.voteId' : voteId},
       //   {'profile.votes.$': 1}
       //   );
-      var profileVoted =  resultsCollection.findOne(
-        {voteId : voteId}
+
+
+      // The below MUST include filter for user
+      // so that multiple ballots from other users
+      // aren't returned for the same vote.
+      var existingBallot =  resultsCollection.findOne(
+        {voteId : voteId, createdBy: Meteor.userId()}
         );
 
-      if (profileVoted) {
+      if (existingBallot) {
         // This user has started this vote already.
         // We will eventually change this to be if(status==="completed")
         // But also we must account for initialization conditions
         console.log("This vote: " + voteId + " already initialized for user: " + Meteor.userId());
-        var voteStatus = profileVoted.status;
+        var voteStatus = existingBallot.status;
         console.log("voter's vote:");
         console.log(voteStatus);
 
@@ -155,6 +184,8 @@ if (Meteor.isClient) {
           // add other conditionals for other statuses. If more than 3, do we want
           // to use switch()? is it worth it now?
           Router.go("doVote", {_id: voteId});
+        } else {
+          console.log("The vote was completed");
         };
 
       } else {
@@ -193,8 +224,14 @@ if (Meteor.isClient) {
         //   orderCurr(array) - the current state of the sort by the user.
         //   status[default=incomplete] - a field to track the status of the vote.
 
-        // Build the object first
+        // NOTE: This is only if ballots are going in
+        // the users collection as a nested object.
+        // The big downside is that you cant use
+        // "dot" concatenation for nested array
+        // filters beyone one level. So you can't do
+        // that if storing ballot in Meteor.users
 
+        // Build the object first
         // var voteObj = {
         //   voteId: voteId,
         //   orderInit: voteShuffle,
@@ -211,7 +248,7 @@ if (Meteor.isClient) {
             choicesInit : voteShuffle,
             createdOn: new Date(),
             createdBy: Meteor.userId(),
-            status: "incomplete",
+            ballotStatus: "incomplete",
             step: 0
           }
         );
