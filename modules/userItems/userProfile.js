9c7188966b034267bf3887173a0b15670c892a6d
robert <robert@monetsu.com>
2016-04-26 18:09:19 +0200

    Implemented round-trip updating and conditionally displaying user profile record fields.

--- modules/userItems/userProfile.js
+++ modules/userItems/userProfile.js
@@ -9,11 +9,15 @@ if (Meteor.isClient) {
     profileData : function () {
       // Get the user's profile data
       // var data = Meteor.user.profile();
-      var data = {userName: "The Boss Man"};
+      var data;
+      var userData = Meteor.user();
+      // if (userData.username) {
+      //   data = {userName: userData.username};
+      // };
 
       // console.log("the user profile data: ");
-      console.log(data);
-      return data;
+      // console.log(userName.username);
+      return userData;
 
     }
 
@@ -23,6 +27,24 @@ if (Meteor.isClient) {
 
     "submit #profileForm" : function (event) {
       event.preventDefault();
+
+      var userName = event.currentTarget.username.value;
+      var userDesc = event.currentTarget.description.value;
+
+      var dataObj = {
+        username: userName,
+        profile: {
+          description: userDesc
+        }
+      };
+
+      Meteor.call("updateUser", dataObj, function (error, result) {
+        if (error) {
+          console.alert(error.reason);
+        };
+      });
+
+      Router.go("userProfile", {_id: Meteor.userId()});
 
     }
 
