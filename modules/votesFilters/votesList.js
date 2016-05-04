8c453641e3da17a73c5cf7ec649918e16f88f3e4
robert <robert@monetsu.com>
2016-05-04 06:57:32 +0200

    Styling filter inputs

--- modules/votesFilters/votesList.js
+++ modules/votesFilters/votesList.js
@@ -1,31 +1,54 @@
+import { ReactiveDict } from 'meteor/reactive-dict'; // for reactive result filtering
+
 if (Meteor.isClient) {
-  Template.myVotes.onCreated(function () {
+  Template.votesList.onCreated(function () {
     var self = this;
 
     self.autorun(function () {
       self.subscribe("votesList");
     });
+
+    this.titles = new ReactiveDict(); // for reactive result title filtering
+
   });
 
   Template.votesList.helpers({
 
     votesList: function () {
-      // Here we will develop filters for searching and displaying different
-      // groups of votes.
-      return votesCollection.find({
+      const instance = Template.instance();
+      
+      const baseQuery = {
         $or: [
-          {voteStatus: "published"},
-          {voteStatus: "closed"}
-        ]},
-        {sort: {publishedOn: -1, createdOn: -1}}).fetch();
-    }
+            {voteStatus: "published"},
+            {voteStatus: "closed"}
+          ]
+        };
+    
+      const sort = {sort: {publishedOn: -1, createdOn: -1}};
+
+      //return all of the votes
+      let query = baseQuery;
+
+      let inputChanged = instance.titles.get('inputChanged');
+      // if input, filter votes
+      if (inputChanged && inputChanged.length > 0) {
+        query = {
+          $and: [baseQuery
+          , {
+            title: new RegExp(inputChanged)
+          }]
+        };
+      }
+
+      return votesCollection.find(query, sort).fetch();
+    } 
 
   });
 
   Template.votesList.events({
-    // code...
+    'input .input-changed input'(event, instance) {
+      instance.titles.set('inputChanged', event.target.value);
+    }
   });
-
-
 
 };
